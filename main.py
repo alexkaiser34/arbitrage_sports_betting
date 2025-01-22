9572b9f3ced692788da8a347f0440350182a683d
Alex Kaiser <alexkaiser@me.com>
2025-01-22 14:07:52 -0500

    updates test algo

--- main.py
+++ main.py
@@ -1,37 +1,7 @@
 from app import App
-import argparse
-import json
-from typing import Dict, Tuple
+from typing import Dict
 from test_algorithm import TestAlgorithm
 
-VALID_APP_TYPES = [ "main", "test" ]
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    # parser.add_argument("sport", help="Specify which sport you are looking at (basketball or football)", type=str)
-    parser.add_argument("app_type", help="Enter application type (main or test)", type=str)
-    args = parser.parse_args()
-    
-    if not args.app_type:
-        print("ERROR: No application type specified - You need the app_type argument")
-        exit(0)
-    elif args.app_type not in VALID_APP_TYPES:
-        print(f'ERROR: {args.app_type} is not a valid application type. Valid types are main or test')
-        exit(0)
-    
-    return args
-
-def read_config(args) -> Tuple[str, Dict]:
-    # read config data (app_config,json)
-    config_data = None
-
-    with open('./app_config.json', 'r') as file:
-        config_data = json.load(file)
-        
-    if args.app_type == "main":
-        return "main", config_data['main_app']
-
-    return "test", config_data['test_algorithm']    
 
 def handle_test(config: Dict):
     testAlgo = TestAlgorithm(
@@ -62,13 +32,18 @@ def handle_main(config: Dict):
     app.run()
 
 def main():
-    args = parse_arguments()
-    app_type, config = read_config(args)
+    wager=int(input('Please enter a wager: '))
+    b1_odds=int(input('Please enter bet1 odds: '))
+    b2_odds=int(input('Please enter bet2 odds: '))
     
-    if app_type == "main":
-        handle_main(config)
-    else:
-        handle_test(config)
+    testAlgo = TestAlgorithm(
+        total_wager=wager,
+        bet1_odds=b1_odds,
+        bet2_odds=b2_odds
+    )
+    
+    testAlgo.run_algorithm()
+    testAlgo.print_winnings()
 
 
 if __name__ == "__main__":

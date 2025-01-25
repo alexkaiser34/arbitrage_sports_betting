eb1852d1eedc112f200710b0386da753aaeef5b7
Alex Kaiser <alexkaiser@me.com>
2025-01-24 19:06:49 -0500

    Adds ability to cache game requests
    Instead of grabbing games every time we run the app,
    lets grab the games once a day at 8 AM.

--- src/models/upcoming_events_response.py
+++ src/models/upcoming_events_response.py
@@ -19,7 +19,21 @@ class UpcomingEventsEndResponse:
         api_time = ciso8601.parse_datetime(self.commence_time).astimezone(UpcomingEventsEndResponse.TIME_ZONE).timestamp()
         now = datetime.now(timezone.utc).astimezone(UpcomingEventsEndResponse.TIME_ZONE).timestamp()
         return (api_time > now)
+    
+    def updateUpcoming(self):
+        self.upcoming = self._isUpcoming()
         
     def __str__(self):
         date = parser.parse(self.commence_time).astimezone(UpcomingEventsEndResponse.TIME_ZONE).strftime("%m-%d %I:%M %p")
         return f'{self.sport_title} - {self.home_team} vs {self.away_team} @ {date}'
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "sports_key": self.sports_key,
+            "sport_title": self.sport_title,
+            "commence_time": self.commence_time,
+            "home_team": self.home_team,
+            "away_team": self.away_team,
+            "upcoming": self.upcoming
+        }

d81b8a3425fd6e8f7079ffd6400afdc486b82806
Alex Kaiser <alexkaiser@me.com>
2025-01-27 21:51:15 -0500

    Restores retreiving configuration from database
    We are unable to store a file in a lambda function
    as the file system is ephemeral

--- src/main.py
+++ src/main.py
@@ -1,29 +1,16 @@
 from app import App
+from dynamodb import DynamoDbConfig, Config
 import os
 import json
 
-class Config:
-    def __init__(self, enabled: bool, live_enabled: bool, sports: str, regions: str, bookmakers: str, wager: str):
-        self.enabled: bool = enabled
-        self.live_enabled: bool = live_enabled
-        self.sports: str = sports
-        self.regions: str = regions
-        self.bookmakers: str = bookmakers
-        self.wager: int = int(wager)
 
-    def __str__(self):
-        return f'enabled={self.enabled},live_enabled={self.live_enabled}sports={self.sports},regions={self.regions},bookmakers={self.bookmakers},wager={str(self.wager)}'
-    
-    def toDict(self):
-        return {
-            "enabled": self.enabled,
-            "live_enabled": self.live_enabled,
-            "sports": self.sports,
-            "regions": self.regions,
-            "bookmakers": self.bookmakers,
-            "wager": self.wager
-        }
+# set this to true when grabbing application config from DynamoDB
+# otherwise, we will use a local file
+# NOTE: Using a local file in a lambda function is not recommended
+# as the file system is ephemeral
+USE_DYNAMODB = True
 
+# helper classes to parse local JSON file config
 class ConfigEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Config):
@@ -43,60 +30,71 @@ class ConfigDecoder(json.JSONDecoder):
         )
 
 def lets_begin(appConfig: Config):
-    if appConfig.enabled:
-        app = App(
-            appConfig.live_enabled,
-            appConfig.sports,
-            appConfig.wager,
-            appConfig.bookmakers,
-            appConfig.regions
-        )
-        app.run()
+    if appConfig is not None:
+        if appConfig.enabled:
+            app = App(
+                appConfig.live_enabled,
+                appConfig.sports,
+                appConfig.wager,
+                appConfig.bookmakers,
+                appConfig.regions
+            )
+            app.run()
+        else:
+            print("App is currently disabled")
     else:
-        print("App is currently disabled")
+        print("Error loading config")
+
 
 def handler(event, context):
 
-    file_path = '/tmp/application_config.json'
+    # use a local file to store application config
+    # this is not recommended in a lambda function
+    if not USE_DYNAMODB:
+        file_path = '/tmp/application_config.json'
 
-    if event and 'update' in event:
-        try:
-            # parse config from event
-            config = Config(
-                event['update']['enabled'],
-                event['update']['live_enabled'], 
-                event['update']['sports'],
-                event['update']['regions'], 
-                event['update']['bookmakers'],
-                event['update']['wager']
-            )
+        if event and 'update' in event:
+            try:
+                # parse config from event
+                config = Config(
+                    event['update']['enabled'],
+                    event['update']['live_enabled'],
+                    event['update']['sports'],
+                    event['update']['regions'],
+                    event['update']['bookmakers'],
+                    event['update']['wager']
+                )
 
-            print("Updating config")
+                print("Updating config")
+                print(config)
+
+                # write config to file
+                with open(file_path, 'w') as config_file:
+                    json.dump(config, config_file, indent=4, cls=ConfigEncoder)
+
+            except Exception as e:
+                print(f"Error updating config: {str(e)}")
+                return
+        else:
+            # regular operation
+            if not os.path.exists(file_path):
+                print("Application is not configured!!")
+                print('Please use admin panel to configure the application')
+                return
+
+            config = None
+            with open(file_path, 'r') as config_file:
+                config = json.load(config_file, cls=ConfigDecoder)
+
+            if config is None:
+                print("Error loading config")
+                return
+
+            print("configured: Config is ")
             print(config)
 
-            # write config to file
-            with open(file_path, 'w') as config_file:
-                json.dump(config, config_file, indent=4, cls=ConfigEncoder)
-
-        except Exception as e:
-            print(f"Error updating config: {str(e)}")
-            return
+            lets_begin(config)
     else:
-        # regular operation
-        if not os.path.exists(file_path):
-            print("Application is not configured!!")
-            print('Please use admin panel to configure the application')
-            return
-
-        config = None
-        with open(file_path, 'r') as config_file:
-            config = json.load(config_file, cls=ConfigDecoder)
-
-        if config is None:
-            print("Error loading config")
-            return
-
-        print("configured: Config is ")
-        print(config)
-
-        lets_begin(config)
+        dynamo = DynamoDbConfig()
+        appConfig = dynamo.getConfig()
+        lets_begin(appConfig)
